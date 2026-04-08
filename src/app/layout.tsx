a7d5650a18df0ed04300a2d1bb5329c369b4cf6e
adakansoftware <adakansoftware@gmail.com>
2026-04-08 23:52:24 +0300

    Add Google Search Console verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ export const metadata: Metadata = {
   metadataBase: getBaseUrl(),
   title: "Adakan",
   description: "Adakan",
+  verification: {
+    google: "F9CjZoLhgyYJb2LPXCUGlNthcunJ53kN_RQqINd2ZUU",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
